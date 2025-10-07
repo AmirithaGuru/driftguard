63e640b7215169f52a863df47c52e6c98e0de167
AmirithaGuru <amirithavarshini.gurupandian@gmail.com>
2025-10-07 00:08:26 -0700

    fix(security): remove intentionally bad 0.0.0.0/0 test rule

--- infra/main.tf
+++ infra/main.tf
@@ -15,6 +15,5 @@ resource "aws_security_group" "bad_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]   # intentionally bad for test
   }
 }
